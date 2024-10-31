0d89e25294fe612579dea651954f472e57a120af
moti <mordechayamramy@gmail.com>
2024-10-31 17:47:50 +0200

    added more bonus questions and more class objects types for it

--- app/db/repository/query_repository.py
+++ app/db/repository/query_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from app.db.database import session_maker
 from app.db.models import Target, City, Country, TargetType
 from app.db.models.mission import Mission
@@ -31,3 +32,48 @@ def get_all_missions():
 def get_mission_result_by_target_type(target_type_name):
     with session_maker() as session:
         return session.query(Mission).join(Mission.targets).join(Target.target_type).filter(TargetType.target_type_name == target_type_name).all()
+
+
+
+def get_mission_statistics_by_city_id(city_id):
+    with session_maker() as session:
+        result = session.query(
+            City.city_id,
+            City.city_name,
+            func.count(Mission.mission_id).label('mission_count'),
+            func.avg(Target.target_priority).label('average_priority')
+        ) \
+        .join(City.targets) \
+        .join(Target.mission) \
+        .filter(City.city_id == city_id) \
+        .group_by(City.city_id, City.city_name) \
+        .first()
+
+        return result
+
+def get_missions_with_high_lost_aircraft(min_lost_aircraft):
+    with session_maker() as session:
+        return session.query(
+            Country.country_id,
+            Country.country_name,
+            func.count(Mission.mission_id).label('total_missions'),
+            func.sum(Mission.aircraft_lost).label('total_lost_aircraft'),
+            func.avg(Mission.aircraft_lost).label('average_lost_aircraft')
+        ).join(Country.cities).join(City.targets).join(Target.mission) \
+            .filter(Mission.aircraft_lost >= min_lost_aircraft) \
+            .group_by(Country.country_id, Country.country_name) \
+            .all()
+
+
+
+def get_missions_by_target_industry_average():
+    with session_maker() as session:
+        return session.query(
+            Target.target_industry,
+            func.count(Mission.mission_id).label('mission_count'),
+            func.sum(Target.target_priority).label('total_priority'),
+            func.avg(Target.target_priority).label('average_priority')
+        ) \
+        .join(Mission.targets) \
+        .group_by(Target.target_industry) \
+        .all()
--- app/gql/query.py
+++ app/gql/query.py
@@ -1,29 +1,51 @@
-from graphene import ObjectType, Field, List, Int, String
-from app.db.database import session_maker
-from app.db.models import City
+from graphene import ObjectType, Field, List, Int, String, Float
 from app.db.repository.mission_repository import get_mission_by_id, get_all_missions
 from app.db.repository.query_repository import get_missions_by_target_industry, \
-    get_missions_by_date_range, get_missions_by_country_name, get_mission_result_by_target_type
-from app.gql.types.city_type import CityType
+    get_missions_by_date_range, get_missions_by_country_name, get_mission_result_by_target_type, \
+    get_mission_statistics_by_city_id, get_missions_with_high_lost_aircraft, get_missions_by_target_industry_average
 from app.gql.types.mission_type import MissionType
 
 
 
+class CityStatisticsType(ObjectType):
+    city_id = Int()
+    city_name = String()
+    mission_count = Int()
+    average_priority = Float()
+
+
+class MissionStatisticsByCountryType(ObjectType):
+    country_id = Int()
+    country_name = String()
+    total_missions = Int()
+    total_lost_aircraft = Float()
+    average_lost_aircraft = Float()
+
+class MissionIndustryStatsType(ObjectType):
+    target_industry = String()
+    mission_count = Int()
+    total_priority = Float()
+    average_priority = Float()
+
+
+
+
 class Query(ObjectType):
-    all_cities = List(CityType)
     mission_by_id = Field(MissionType, mission_id=Int(required=True))
     missions_by_date_range = List(MissionType, start_date=String(required=True), end_date=String(required=True))
     missions_by_target_industry = List(MissionType, target_industry=String(required=True))
     missions_by_country_name = List(MissionType, country_name=String(required=True))
     all_missions = List(MissionType)
     mission_result_by_target_type = List(MissionType, target_type=String(required=True))
-
-
-    @staticmethod
-    def resolve_all_cities(root, info):
-        with session_maker() as session:
-            return session.query(City).all()
-
+    mission_statistics_by_city = Field(
+        CityStatisticsType,
+        city_id=String(required=True)
+    )
+    missions_with_high_lost_aircraft = List(
+        MissionStatisticsByCountryType,
+        min_lost_aircraft=Float(required=True)
+    )
+    get_all_missions_by_target_industry_average = List(MissionIndustryStatsType)
 
 
     @staticmethod
@@ -49,3 +71,32 @@ class Query(ObjectType):
     @staticmethod
     def resolve_mission_result_by_target_type(root, info, target_type):
         return get_mission_result_by_target_type(target_type)
+
+    @staticmethod
+    def resolve_mission_statistics_by_city(root, info, city_id):
+        stats = get_mission_statistics_by_city_id(city_id)
+        print(stats)
+        if stats:
+            return {
+                'city_id': stats.city_id,
+                'city_name': stats.city_name,
+                'mission_count': stats.mission_count,
+                'average_priority': stats.average_priority
+            }
+        return None
+
+    @staticmethod
+    def resolve_missions_with_high_lost_aircraft(root, info, min_lost_aircraft):
+        return get_missions_with_high_lost_aircraft(min_lost_aircraft)
+
+
+
+    @staticmethod
+    def resolve_get_all_missions_by_target_industry_average(root, info):
+        stats = get_missions_by_target_industry_average()
+        return [{
+            'target_industry': stat.target_industry,
+            'mission_count': stat.mission_count,
+            'total_priority': stat.total_priority,
+            'average_priority': stat.average_priority
+        } for stat in stats]
